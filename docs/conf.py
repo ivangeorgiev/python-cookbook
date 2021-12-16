d39c0dc4ea67c463ca18aff1009e2f0d8631bf90
Ivan Georgiev <ivan.georgiev@gmail.com>
2021-12-16 13:05:22 +0200

    adding .readthedocs.yaml

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-# html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_book_theme'
 
 RTD_NEW_THEME = True
 
